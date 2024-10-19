f9331d8b57ff36c702b0486e76366b808e814c94
woosungking <abuga991008@gmail.com>
2024-10-18 23:47:41 -0700

    설정

---
+++ src/test/java/com/java/experiment/JavaExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.experiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExperimentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
